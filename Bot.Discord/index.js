149e129fda72fe65274ef0ff3d082ff8e8ce9a2d
yaniscones <yanis.cones@orange.fr>
2026-02-19 20:41:11 +0100

    Update index.js

--- Bot.Discord/index.js
+++ Bot.Discord/index.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const axios = require('axios');
 
-const TARGET_CHANNEL_ID = "1379835906160201728"; // ID du bug-report
+const TARGET_CHANNEL_ID = "1379835906160201728"; 
 const N8N_WEBHOOK = process.env.N8N_WEBHOOK_URL;
 
 const client = new Client({
@@ -12,11 +12,9 @@ const client = new Client({
 
 client.on('threadCreate', async (thread) => {
 
-  // 🔒 On ne prend que les threads du channel bug-report
   if (!thread.parent || thread.parent.id !== TARGET_CHANNEL_ID) return;
 
   try {
-    // petite attente pour que le message starter soit dispo
     await new Promise(resolve => setTimeout(resolve, 1000));
 
     const firstMessage = await thread.fetchStarterMessage();
@@ -44,3 +42,4 @@ client.once('ready', () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
